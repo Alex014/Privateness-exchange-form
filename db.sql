75bed51f5b6f52afb1df9c13421318bbdaa8db3c
Aleksej Sokolov <aleksej000@gmail.com>
2022-05-13 00:05:47 +0000

    Table upgrade

--- db.sql
+++ db.sql
@@ -1,40 +1,28 @@
 -- --------------------------------------------------------
 
---
--- Table structure for table `tokens`
---
-
 CREATE TABLE `tokens` (
   `ID` int(10) UNSIGNED NOT NULL,
   `content` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `address` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `pay_address` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `gen_address` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `hours` int(11) DEFAULT NULL,
   `error` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `status` enum('FOUND','PARSED','ERROR','ACTIVATED','PAYED') COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `status` enum('PARSED','CHECKED','ACTIVATED','PAYED','ERROR') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `crc32` bigint(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `tokens`
---
 ALTER TABLE `tokens`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `address` (`address`),
-  ADD UNIQUE KEY `pay_address` (`pay_address`),
   ADD UNIQUE KEY `gen_address` (`gen_address`),
-  ADD KEY `status` (`status`);
+  ADD KEY `status` (`status`),
+  ADD KEY `hours` (`hours`),
+  ADD KEY `crc32` (`crc32`),
+  ADD KEY `pay_address` (`pay_address`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `tokens`
---
 ALTER TABLE `tokens`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
