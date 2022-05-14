f3ce4735570f4e619038be4efbbe6ff19d4f489e
Aleksej Sokolov <aleksej000@gmail.com>
2022-05-14 02:33:46 +0000

    Search form and show token

--- web/index.php
+++ web/index.php
@@ -1,8 +1,57 @@
-<html>
-    <head>
-        <title>Privateness Exchange form</title>
-    </head>
-    <body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </body>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Privateness Exchange Form</title>
+  </head>
+  <body>
+
+  <div class="container">
+  <div class="row">
+    <div class="col">
+        <p>
+            Create NVS record in Emercoin <br>
+            key: <b>worm:token:ness_exchange_v1_v2:your_address_address-v1-with-coinhours</b> <br>
+            value: 
+            <pre>
+                <code>
+                    <?=htmlentities('
+<worm>
+    <token type="ness-exchange-v1-v2" address="your_address_address-v1-with-coinhours" pay_address="your_address_to-recieve-coins-v2"/>
+</worm>');?>
+                </code>
+            </pre>
+        </p>
+
+        <form action="/token.php" method="GET">
+        <div class="mb-3">
+            <label for="token" class="form-label">Your token address</label>
+            <input type="text" class="form-control" id="token" name="address" placeholder="your_address_address-v1-with-coinhours">
+            <div id="emailHelp" class="form-text">The address in privateness v1 network, where you have coin-hours.</div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Find</button>
+        </form>
+    
+    </div>
+  </div>
+  </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
--- web/token.php
+++ web/token.php
@@ -1,10 +1,94 @@
 <?php
+require '../lib/DB.php';
 
-foreach ($_GET as $param => $value) {
-    $token = $param;
-    break;
-}
+ini_set('display_errors', true);
 
-if (isset($token)) {
-    echo $token;
-}
+use lib\DB;
+
+$config = require '../config/config.php';
+$db = new DB($config['db']['host'], $config['db']['database'], $config['db']['user'], $config['db']['password']);
+
+var_dump($_GET);
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Exchange token</title>
+  </head>
+  <body>
+
+
+  <div class="container">
+  <div class="row">
+    <div class="col">
+
+    <?php if (isset($_GET['address'])): ?>
+<?php
+$address = $_GET['address'];
+$token = $db->find($address);
+$status = $token['status'];
+?>
+        <h4><?=$token['status']?></h4>
+        <pre><code><?=htmlentities($token['content'])?></code></pre>
+
+        <table class="table table-bordered">
+            <tr>
+                <th>Address</th>
+                <th>Pay address</th>
+                <th>Generated address</th>
+            </tr>
+            <tr>
+                <td><?=$token['address']?></td>
+                <td><?=$token['pay_address']?></td>
+                <td><?=$token['gen_address']?></td>
+            </tr>
+        </table>
+
+        <?php if ('CHECKED' === $status): ?>
+            <p>Your token is checked</p>
+        <?php elseif ('ACTIVATED' === $status): ?>
+            <p>Your token is activated</p>
+            <p>Your have <b><?=$token['hours']?></b> HOURS on <b><?=$token['address']?></b> (v1)</p>
+            <p>Transmit any ammount (0.000001) 
+                from <b><?=$token['address']?></b> (v2) 
+                to <b><?=$token['gen_address']?></b> (v2) 
+                and you will recieve <?=$hours  / $config['exchange']['ratio']?> NESS on your address <b><?=$token['pay_address']?></b> (v2) </p>
+        <?php elseif ('PAYED' === $status): ?>
+            <p>Your token is payed</p>
+            <p><?=$hours  / $config['exchange']['ratio']?> NESS</p>
+            <p>Check your balance at <b><?=$token['pay_address']?></b> (v2) </p>
+        <?php endif; ?>
+    <?php else: ?>
+        <h1>The address is not given !</h1>
+        <h3>    
+            Use <code>token.php?address=your_privateness_token_address</code>
+        </h3>
+    <?php endif; ?>
+
+    
+    </div>
+  </div>
+  </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
+
