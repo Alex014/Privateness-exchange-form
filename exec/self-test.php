08108d95c8e0dd3b24302e6fc382c315d6acf03b
Aleksej Sokolov <aleksej000@gmail.com>
2022-05-19 02:57:14 +0000

    Mare parameters for self-test

--- exec/self-test.php
+++ exec/self-test.php
@@ -17,6 +17,12 @@ if (!file_exists(__DIR__ . '/../config/config.php')) {
 
 $config = require '../config/config.php';
 
+$ness1 = $config['ness']['v1'];
+$ness2 = $config['ness']['v2'];
+
+$v1 = new Ness($ness1['host'], (int) $ness1['port'], $ness1['wallet_id'], $ness1['password']);
+$v2 = new Ness($ness2['host'], (int) $ness2['port'], $ness2['wallet_id'], $ness2['password']);
+
 if ($argc > 1) {
     if ('-a' === $argv[1]) {
         echo "\n" . $config['ness']['v2']['payment_address'] . "\n";
@@ -26,6 +32,10 @@ if ($argc > 1) {
         print_r($config['ness']['v1']);
     } elseif ('-v2' === $argv[1]) {
         print_r($config['ness']['v2']);
+    } elseif ('-info1' === $argv[1]) {
+        print_r($v1->health());
+    } elseif ('-info2' === $argv[1]) {
+        print_r($v2->health());
     } elseif ('-emc' === $argv[1]) {
         print_r($config['emercoin']);
     } elseif ('-db' === $argv[1]) {
@@ -44,12 +54,6 @@ Emercoin::$port = $config['emercoin']['port'];
 Emercoin::$username = $config['emercoin']['user'];
 Emercoin::$password = $config['emercoin']['password'];
 
-$ness1 = $config['ness']['v1'];
-$ness2 = $config['ness']['v2'];
-
-$v1 = new Ness($ness1['host'], (int) $ness1['port'], $ness1['wallet_id'], $ness1['password']);
-$v2 = new Ness($ness2['host'], (int) $ness2['port'], $ness2['wallet_id'], $ness2['password']);
-
 if (is_array($db->findAll())) {
     echo "\nDB - OK\n";
 } else {
@@ -59,14 +63,14 @@ if (is_array($db->findAll())) {
 try {
     Emercoin::getinfo();
 } catch (\Exception $err) {
-    echo "\nEmercoin NVS - FAILED\n";
+    echo "\nEmercoin NVS - FAILED (" . $err->getMessage() . ")\n";
 }
 
 if (!isset($err)) {
     echo "\nEmercoin NVS - OK\n";
 }
 
-if ($v2->health()) {
+if ($v1->health()) {
     echo "\nNESS V1 - OK\n";
 } else {
     echo "\nNESS V1 - FAILED\n";
@@ -80,10 +84,12 @@ if ($v2->checkAddress($ness2['payment_address'])) {
 
 echo "
 CMD PARAMS:
-self-test.php -a    show payment address
-self-test.php -v1   show Privateness network V1 config
-self-test.php -v2   show Privateness network V2 config
-self-test.php -emc  show EMERCOIN config
-self-test.php -db   show database config
-self-test.php -v    show all config
+self-test.php -a        show payment address
+self-test.php -v1       show Privateness network V1 CONFIG
+self-test.php -v2       show Privateness network V2 CONFIG
+self-test.php -info1    show Privateness network V1 INFORMATION
+self-test.php -info2    show Privateness network V2 INFORMATION
+self-test.php -emc      show EMERCOIN config
+self-test.php -db       show database config
+self-test.php -v        show all config
 ";
