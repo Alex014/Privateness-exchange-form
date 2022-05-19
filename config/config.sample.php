225bca4905322640eb15ef62045c7ac6f6a6aa18
Aleksej Sokolov <aleksej000@gmail.com>
2022-05-19 01:09:41 +0000

    Readme

---
+++ config/config.sample.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'ness' => [
+        'v1' => [
+            'host' => 'localhost',
+            'port' => '6660',
+            'wallet_id' => '',
+            'password' => ''
+        ],
+        'v2' => [
+            'host' => 'localhost',
+            'port' => '6660',
+            'wallet_id' => '',
+            'password' => '',
+            'payment_address' => ''
+        ],
+        'vvv' => [
+            'host' => 'localhost',
+            'port' => '6660',
+            'wallet_id' => '',
+            'password' => ''
+        ]
+    ],
+    'emercoin' => [
+        'host' => '127.0.0.1',
+        'port' => '8332',
+        'user' => '',
+        'password' => ''
+    ],
+    'db' => [
+        'host' => 'localhost',
+        'user' => '',
+        'password' => '',
+        'database' => ''
+    ],
+    'exchange' => [
+        'ratio' => 200000
+    ]
+];
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Privateness v1 => v2 exchange form
+
+## Exchange COIN-HOURS from v1 to COINS v2
+
+Execute
+
+`mv config/config.sample.php config/config.php`
+
+Edit `config.php`
