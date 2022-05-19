07e6c0512d7c2675854f552ec1561b64a7edc4b8
Aleksej Sokolov <aleksej000@gmail.com>
2022-05-19 02:39:51 +0000

    Self test CMD params

--- exec/self-test.php
+++ exec/self-test.php
@@ -11,11 +11,31 @@ use lib\Emercoin;
 ini_set('display_errors', true);
 error_reporting(E_ERROR);
 
-if (!file_exists(__DIR__ . '../config/config.php')) {
+if (!file_exists(__DIR__ . '/../config/config.php')) {
     die ('config.php file does not exist');
 }
 
 $config = require '../config/config.php';
+
+if ($argc > 1) {
+    if ('-a' === $argv[1]) {
+        echo "\n" . $config['ness']['v2']['payment_address'] . "\n";
+    } elseif ('-v' === $argv[1]) {
+        print_r($config);
+    } elseif ('-v1' === $argv[1]) {
+        print_r($config['ness']['v1']);
+    } elseif ('-v2' === $argv[1]) {
+        print_r($config['ness']['v2']);
+    } elseif ('-emc' === $argv[1]) {
+        print_r($config['emercoin']);
+    } elseif ('-db' === $argv[1]) {
+        print_r($config)['db'];
+    } else {
+        print_r($config);
+    }
+
+    die();
+}
 
 $db = new DB($config['db']['host'], $config['db']['database'], $config['db']['user'], $config['db']['password']);
 
@@ -24,7 +44,7 @@ Emercoin::$port = $config['emercoin']['port'];
 Emercoin::$username = $config['emercoin']['user'];
 Emercoin::$password = $config['emercoin']['password'];
 
-$ness1 = $config['ness']['v2'];
+$ness1 = $config['ness']['v1'];
 $ness2 = $config['ness']['v2'];
 
 $v1 = new Ness($ness1['host'], (int) $ness1['port'], $ness1['wallet_id'], $ness1['password']);
@@ -57,3 +77,13 @@ if ($v2->checkAddress($ness2['payment_address'])) {
 } else {
     echo "\nNESS V2 - FAILED\n";
 }
+
+echo "
+CMD PARAMS:
+self-test.php -a    show payment address
+self-test.php -v1   show Privateness network V1 config
+self-test.php -v2   show Privateness network V2 config
+self-test.php -emc  show EMERCOIN config
+self-test.php -db   show database config
+self-test.php -v    show all config
+";
